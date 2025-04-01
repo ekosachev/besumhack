e66e1dd0e70434e043b03d1a5746b3784d28fda6
Vinermy <egorkosachev@vk.com>
2025-04-01 16:06:11 +0300

    added index and replaced links with tags

--- notmeat/settings.py
+++ notmeat/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = "notmeat.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": ["../src"],
+        "DIRS": ["src/assets"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [BASE_DIR / "dist"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- notmeat_app/urls.py
+++ notmeat_app/urls.py
@@ -1,1 +1,6 @@
-urlpatterns = []
+from django.urls import path
+
+from notmeat_app import views
+
+
+urlpatterns = [path("", views.index, name="index")]
